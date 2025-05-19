e377b6b410a108c05493ab7542822fde6fec2c06
SalvatoreSacco <salvatoresacco02@gmail.com>
2025-05-19 19:08:33 +0200

    esercizi

---
+++ esercizio1.ts
@@ -0,0 +1,5 @@
+let nome:string = "Salvatore";
+let eta: number = 23;
+let isStudent: boolean = true;
+let hobby:Array<string> = ["calcio","chitarra"];
+console.log ("Ciao mi chiamo "  + nome +  " ho " +  eta  +  " anni e i miei "  +  " hobby sono " + hobby);
---
+++ esercizio2.ts
@@ -0,0 +1,9 @@
+const temperatura = 15;
+
+if (temperatura > 30) {
+  console.log("fa caldo");
+} else if (temperatura < 10) {
+  console.log("fa freddo");
+} else {
+  console.log("temperatura ideale");
+}
